29bc6e41512951f15aa0472817ac80ec340e0af9
StephenThompson <stephenthomp09@gmail.com>
2016-05-05 20:31:48 +1200

    Got MongoDB working and storing registered users. Instructions on how to set up mongodb can be found at the top of the account.js file.

--- routes/account.js
+++ routes/account.js
@@ -1,23 +1,88 @@
 var express = require('express');
 var router = express.Router();
 
+var MongoClient = require('mongodb').MongoClient;
+
+// Connection URL
+var url = 'mongodb://localhost:27017/SWEN303';
+
+////////////////////////////////////////////////////////////////////////////////////
+/* Instructions to get Mongo working on Vic lab computers
+1. Download the correct version from https://www.mongodb.org/downloads
+2. Extract to any folder
+3. In the extracted folder, create a new folder called "data" (this will hold the data stored in the database)
+4. Open a terminal in the extracted folder
+5. Run the command "./bin/mongod --storageEngine mmapv1 --dbpath=./data --port 27017" to start the database server
+
+The website should now be able to connect to the database
+*///////////////////////////////////////////////////////////////////////////////////
+
+
+
 // This method doesn't display a page, rather it tries to log in a member
 // and sends a reply back to the client
-router.post('/validate', function(req, res) {
+router.post('/validate', function (req, res) {
     var username = req.query.username;
     var password = req.query.password;
 
-    if (username == "admin" && password == "123") {
-        res.cookie('isLoggedIn', username, {overwrite: true, httpOnly: false});
-        res.send("true");
-    } else {
-        res.send("false");
-    }
+    // Use connect method to connect to the Server
+    MongoClient.connect(url, function (err, db) {
+        if (err) {
+            throw err;
+        }
+        db.collection('users').find({"username": username, "password": password}).toArray(function (err, result) {
+            if (err) {
+                throw err;
+            }
+            if (result.length > 0) {
+                res.cookie('isLoggedIn', username, {overwrite: true, httpOnly: false});
+                res.send("true");
+            } else {
+                console.log("Loggin failed");
+                res.send("false");
+            }
+        });
+    });
+
+
 });
 
-router.post('/logout', function(req, res) {
+router.post('/logout', function (req, res) {
     res.clearCookie('isLoggedIn');
     res.send("done");
 });
+
+router.post('/register', function (req, res) {
+    var firstname = req.query.firstname;
+    var lastname = req.query.lastname;
+    var username = req.query.username;
+    var password = req.query.password;
+
+    // Use connect method to connect to the Server
+    MongoClient.connect(url, function (err, db) {
+        if (err) {
+            console.error(err);
+            throw err;
+        }
+        console.log("Logged in");
+
+        db.collection('users').insert(
+            {"username": username, "password": password, "firstname": firstname, "lastname": lastname},
+            function (err, result) {
+                console.log("Inserted 3 documents into the document collection");
+
+                if (!err) {
+                    console.log("New user added");
+                    res.cookie('isLoggedIn', username, {overwrite: true, httpOnly: false});
+                    res.send("true");
+                } else {
+                    console.log("Registration failed");
+                    res.send("false");
+                }
+            });
+    });
+
+});
+
 
 module.exports = router;
