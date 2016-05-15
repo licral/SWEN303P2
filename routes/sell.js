7747c8162a78d3638e611780d7d06f6203774690
Stevie Kaye <stevie.kaye@gmail.com>
2016-05-16 10:15:44 +1200

    Added form on sell page

--- routes/sell.js
+++ routes/sell.js
@@ -10,3 +10,34 @@ router.get('/', function(req, res, next) {
 });
 
 module.exports = router;
+
+
+
+// var Listing = new Schema({
+// ID : Number
+// Seller : ObjectId
+// ItemName : String
+// Category : ObjectId
+// Price : Number
+// Date : Date
+// Photograph : ObjectId?
+// Description : String
+// });
+
+// app.post('/sell', function(req, res){
+//     new Listing({
+//         ID : // randomly generated? #entry in DB?
+//         Seller  : (get username)
+//         ItemName : req.body.sellItemName
+//         Category : req.body.category
+//         Price   : req.body.sellPrice
+//         Date : (get date)
+//         Photograph : (get photo???)
+//         Description : req.body.itemDescription
+//
+//
+//     }).save(function(err, prd){
+//         if(err) res.json(err);
+//         else    res.send("Item successfully listed!");
+//     });
+// });
