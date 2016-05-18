47c10fbae56e2ce35c6f4b54cb49b9e79276112c
Bonnie Liao <kurousagiwasugoi@gmail.com>
2016-05-18 16:34:55 +1200

    UPLOAD IMAGE WORKS OMG

--- routes/sell.js
+++ routes/sell.js
@@ -1,15 +1,31 @@
 var express = require('express');
 var router = express.Router();
 
+// Connection URL
+var url = 'mongodb://localhost:27017/SWEN303';
+
 var multer = require('multer');
 var storage = multer.memoryStorage();
 var upload = multer({storage: storage});
 router.use(upload.single('image'));
 
-var MongoClient = require('mongodb').MongoClient;
+var fs = require('fs');
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
 
-// Connection URL
-var url = 'mongodb://localhost:27017/SWEN303';
+mongoose.connect(url);
+
+var schema = new Schema({
+    img: {
+        id: Number,
+        data: Buffer,
+        contentType: String
+    }
+});
+
+var A = mongoose.model('image', schema);
+
+var MongoClient = require('mongodb').MongoClient;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -20,8 +36,27 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/add_item', function(req, res){
-    res.redirect("/");
-})
+    var a = new A();
+    console.log(req.file);
+    a.img.data = req.file.buffer;
+    a.img.contentType = 'image/jpg';
+    a.save(function(err){
+        if(err) throw err;
+        console.log('saved img to mongo');
+        res.redirect("/sell/uploads/fullsize/" + a._id);
+    });
+});
+
+// METHOD OF GETTING THE IMAGE AND RETURNING IT
+router.get('/uploads/fullsize/:id', function(req, res){
+    id = req.params.id;
+    A.find({'_id' : id}, 'img.data', function(err, data){
+        console.log(data[0].img.data);
+        var img = data[0].img.data;
+        res.writeHead(200, {'Content-Type' : 'image/jpg'});
+        res.end(img, 'binary');
+    });
+});
 
 module.exports = router;
 
