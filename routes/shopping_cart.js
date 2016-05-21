68a6768f3eca3824cd41500554a78c80f3ee3d1f
StephenThompson <stephenthomp09@gmail.com>
2016-05-21 15:37:44 +1200

    The shopping cart mostly works now. Still to add: Remove, update the price/menubar when the quantity changes.

--- routes/shopping_cart.js
+++ routes/shopping_cart.js
@@ -1,12 +1,49 @@
 var express = require('express');
 var router = express.Router();
 
+var MongoClient = require('mongodb').MongoClient;
+var ObjectId = require('mongodb').ObjectId;
+var Item = require('./mongoose');
+
+// Connection URL
+var url = 'mongodb://localhost:27017/SWEN303';
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    console.log("Cookies: ", req.cookies);
-    res.render('shopping_cart', {
-        title: 'Express',
-        page : 'Shopping Cart'
+    var username = req.cookies.isLoggedIn;
+    // Use connect method to connect to the Server
+    MongoClient.connect(url, function (err, db) {
+        if (err) {
+            throw err;
+        }
+        // Get items array in cart
+        db.collection('users').find({"username": username}).toArray(function (err, result) {
+            if (err) {
+                throw err;
+            }
+            console.log(result);
+            // Convert results to objectid array
+            var myArray = [];
+            var myQuantity = [];
+            for(var i=0; i<result[0].items_In_Cart.length; i++) {
+                myArray.push(ObjectId(result[0].items_In_Cart[i].itemID));
+                myQuantity.push(Number(result[0].items_In_Cart[i].quantity));
+            }
+            
+            // Find items
+            db.collection('items').find({_id: { $in: myArray}}).toArray(function (err2, resultItems) {
+                if (err2) {
+                    throw err2;
+                }
+                console.log("Result: " + resultItems);
+                res.render('shopping_cart', {
+                    title: 'Express',
+                    page : 'Shopping Cart',
+                    items: resultItems,
+                    quantityList: myQuantity
+                });
+            });
+        });
     });
 });
 
