cc8c2cf5ea1a5e9117eb5263b0030133bce81b31
Bonnie Liao <kurousagiwasugoi@gmail.com>
2016-05-21 14:36:39 +1200

    Favourite now worksgit status! :D

--- routes/favourites.js
+++ routes/favourites.js
@@ -1,15 +1,37 @@
 var express = require('express');
 var router = express.Router();
 
+var MongoClient = require('mongodb').MongoClient;
+
+var Item = require('./mongoose');
+
+// Connection URL
+var url = 'mongodb://localhost:27017/SWEN303';
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('favourites', {
-        title: 'Express',
-        page : 'Favourites'
+    var username = req.cookies.isLoggedIn;
+    MongoClient.connect(url, function (err, db) {
+        if (err) {
+            throw err;
+        }
+        db.collection('users').find({"username": username}).toArray(function (err, result) {
+            if (err) {
+                throw err;
+            }
+            console.log("Hellowwwwwwww")
+            console.log(result[0]);
+            console.log("Worldddddddd")
+            res.render('favourites', {
+                title: 'Express',
+                page: 'Favourites',
+                items: result[0].favourites
+            });
+        });
     });
 });
 
-router.post('/addItemToCart', function (req, res) {
+router.post('/add', function (req, res) {
     var username = req.cookies.isLoggedIn;
     var item = req.query.item;
     // Use connect method to connect to the Server
@@ -17,9 +39,8 @@ router.post('/addItemToCart', function (req, res) {
         if (err) {
             throw err;
         }
-        console.log("2");
-        //db.collection('users').update({"username": username}, { $push: {items_In_Cart: item}});
-        console.log("success");
+        db.collection('users').update({"username": username}, { $push: {favourites: item}});
+        console.log("Added to Favourites");
         res.send("success");
     });
 });
