bdad87fa13f10d40b19e5e9cc50b2136a08c7091
Stevie Kaye <stevie.kaye@gmail.com>
2016-05-21 16:13:40 +1200

    Moved categories from sell.ejs to a categories.js for sitewide access.

---
+++ views/include/categories.js
@@ -0,0 +1,19 @@
+
+
+var categories = {
+    "Clothing": {
+        "Men": ["Shirts", "Hoodies & Sweatshirts", "Jackets & Coats", "Sweaters", "Costumes", "Activewear", "Trousers", "Socks", "Suits & Sport Coats", "Vests", "Jeans", "Shorts", "Underwear", "Swim Trunks", "Pyjamas & Robes", "Ponchos", "Kilts"],
+        "Women": ["Tops & Tees", "Dresses", "Lingerie", "Jackets & Coats", "Skirts", "Sweaters", "Hoodies & Sweatshirts", "Swimwear", "Costumes", "Trousers & Capris", "Socks & Hosiery", "Shorts & Skorts", "Pyjamas & Robes", "Activewear", "Leggings", "Jeans", "Ponchos", "Jumpsuits & Rompers", "Blazers", "Suits", "Bodysuits", "Overalls"],
+        "Gender diverse": ["Tops & Tees", "Lingerie", "Jackets & Coats", "Skirts", "Sweaters", "Hoodies & Sweatshirts", "Swimwear", "Costumes", "Trousers & Capris", "Shorts & Skorts", "Pyjamas & Robes", "Jeans", "Ponchos", "Jumpsuits & Rompers", "Blazers", "Suits", "Bodysuits", "Overalls"],
+        "Children": ["Baby Clothes", "Tops", "Bodysuits", "Trousers", "Costumes", "Clothing Sets", "Sweaters", "Hoodies & Sweatshirts", "Jackets & Coats", "Socks", "Shorts", "Activewear", "Pyjamas & Robes", "Underwear", "Swimwear", "Footies & Rompers", "Suits", "Ponchos", "Vests"]
+    },
+    "Technology": {
+        "Gadgets": ["Binoculars", "Cameras"],
+        "Computers": ["Desktops", "Laptops"]
+    },
+    "Arts and Crafts": {
+        "Jewellery": ["Earrings", "Necklaces", "Rings"],
+        "Fabric": ["Quilts", "Embroidery", "Crochet"],
+        "Craft supplies": ["Knitting", "Bakeware", "Jewellery and beading"]
+    }
+}
