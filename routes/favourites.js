2a7c4112cc4f3574454ca0d3049584f284a3c191
Bonnie Liao <kurousagiwasugoi@gmail.com>
2016-05-21 13:55:47 +1200

    Attempting favourites

--- routes/favourites.js
+++ routes/favourites.js
@@ -9,4 +9,19 @@ router.get('/', function(req, res, next) {
     });
 });
 
+router.post('/addItemToCart', function (req, res) {
+    var username = req.cookies.isLoggedIn;
+    var item = req.query.item;
+    // Use connect method to connect to the Server
+    MongoClient.connect(url, function (err, db) {
+        if (err) {
+            throw err;
+        }
+        console.log("2");
+        //db.collection('users').update({"username": username}, { $push: {items_In_Cart: item}});
+        console.log("success");
+        res.send("success");
+    });
+});
+
 module.exports = router;
