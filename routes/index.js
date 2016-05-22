9fa646609dc57d14a5eee9e6e49499cb89436d9b
Stevie Kaye <stevie.kaye@gmail.com>
2016-05-22 17:20:33 +1200

    Minor changes to home page; passing in # of users & items in DB.

--- routes/index.js
+++ routes/index.js
@@ -3,17 +3,35 @@ var router = express.Router();
 
 var Item = require('./mongoose');
 
+var MongoClient = require('mongodb').MongoClient;
+
+// Connection URL
+var url = 'mongodb://localhost:27017/SWEN303';
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    Item.find({}, function(err, data){
-        console.log(data);
-        res.render('index', {
-            title: 'Express',
-            page : 'Home',
-            items: data
-        });        
-    }).sort({$natural : -1}).limit(5);
-    
+    var Users;
+    MongoClient.connect(url, function (err, db) {
+        if (err) {
+            console.error(err);
+            throw err;
+        }
+        Item.count({}, function(error, numOfItems) {
+            db.collection('users').count({}, function(error, numOfUsers) {
+                Item.find({}, function(err, data){
+                    res.render('index', {
+                        title: 'Express',
+                        page : 'Home',
+                        items: data,
+                        users: numOfUsers,
+                        itemCount: numOfItems
+                    });
+                }).sort({$natural : -1}).limit(5);
+            });
+
+        });
+    })
 });
 
 module.exports = router;
