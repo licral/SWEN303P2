20fa798cc6842fe16839ef285d77aa41f8bbdfeb
Bonnie Liao <kurousagiwasugoi@gmail.com>
2016-05-23 20:11:04 +1200

    Made the list of items in search and favourites look like the browse

--- routes/search.js
+++ routes/search.js
@@ -4,16 +4,22 @@ var router = express.Router();
 var Item = require('./mongoose');
 
 /* GET home page. */
-router.post('/', function(req, res, next) {
+router.post('/:page', function(req, res, next) {
+    var page = req.params.page;
     var search = req.body.search;
-    Item.find({'title' : {'$regex' : search, '$options' : 'i'}}, function(err, result){
-        console.log(result);
-        if (err) throw err;
-        res.render('search', {
-            title: 'Express',
-            page : 'Results',
-            items : result,
-            query : search
+    var skipVal = ((page-1) * 10);
+
+    Item.find({'title' : {'$regex' : search, '$options' : 'i'}}).skip(skipVal).limit(20).sort({title:1}).exec(function(err, items){
+        Item.count({'title' : {'$regex' : search, '$options' : 'i'}}, function(err, count){
+            if (err) throw err;
+            res.render('search', {
+                title: 'Express',
+                page : 'Results',
+                items:  items,
+                count: count,
+                start: skipVal,
+                query: search
+            });
         });
     });
 });
